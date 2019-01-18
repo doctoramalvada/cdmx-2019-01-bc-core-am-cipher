e49ac869c7b4fdd58358a9b604bbb9bdab0dc62f
doctoramalvada <mydemoni@hotmail.com>
2019-01-18 13:09:22 -0600

    pasaron los test

--- src/cipher.js
+++ src/cipher.js
@@ -2,7 +2,7 @@ window.cipher = {
  encode: (num,text)=>{
    let str = text.value;
    let mayusculas= str.toUpperCase();
-   offset = num.value;
+   let offset = num.value;
    /* defino la funcion encode que consta del numer y texto ingresados 
    con texto.value creo una nueva variable a la que volvere a mayusculas las letras
    tambien obtengo el offset que es un parametro para la funcion de conversion de ascci a letras
@@ -11,11 +11,10 @@ window.cipher = {
 /*aqui saldra el texto cifrado */
    for(let i=0; i<mayusculas.length; i++){
      let letraAscii= mayusculas.charCodeAt(i);
-     console.log(letraAscii);
      let conversion=(letraAscii-65+parseInt(offset))%26+65;
-     console.log(conversion);
-     let obtenido=String.fromCharCode(conversion);
-     console.log(obtenido);
+     
+let obtenido=String.fromCharCode(conversion);
+     
      textNuevo += obtenido;
     }
     /*el charcodeat me da el valor numerico  de la letra ingresadapongo la funcion para que convierta las letras en ascci */
@@ -33,7 +32,7 @@ window.cipher = {
      
      let conversionde=(letraAscii+65-parseInt(offset))%26+65;
      let listode=String.fromCharCode(conversionde);
-     console.log(listode);
+    
      resultadodescifrado += listode;
    }
 
